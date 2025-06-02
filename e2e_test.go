3f00ff19d325b35ffc366b4df961041763274dd8
Andrew <a@mevton.com>
2025-06-02 14:14:16 +0200

    Fix e2e tests

--- e2e_test.go
+++ e2e_test.go
@@ -11,7 +11,11 @@ import (
 	sova "github.com/sova-network/sova-sdk-go"
 )
 
-func TestSuccessfulClient(t *testing.T) {
+func TestTestnetSuccessfulClient(t *testing.T) {
+	ctx := context.Background()
+	subCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
 	client := sova.NewTestnetClient()
 
 	privKeyB64 := "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
@@ -19,7 +23,6 @@ func TestSuccessfulClient(t *testing.T) {
 	privateKey, err := base64.StdEncoding.DecodeString(privKeyB64)
 	assert.NoError(t, err)
 
-	ctx := context.Background()
 	// Authenticate
 	token, err := client.Authenticate(ctx, privateKey)
 
@@ -37,15 +40,15 @@ func TestSuccessfulClient(t *testing.T) {
 	resultCh := make(chan *types.BundleResult, 1)
 
 	// Subscribe to mempool packets
-	err = searcher.SubscribeByWorkchain(ctx, 0, func(mp *types.MempoolPacket) {
-		t.Log("reseive mempool packet")
+	err = searcher.SubscribeByWorkchain(subCtx, 0, func(mp *types.MempoolPacket) {
+		t.Log("receive mempool packet")
 		mempoolCh <- mp
 	})
 	assert.NoError(t, err)
 
 	// Subscribe to bundle results
-	err = searcher.SubscribeBundleResult(ctx, func(br *types.BundleResult) {
-		t.Log("reseive bundle result")
+	err = searcher.SubscribeBundleResult(subCtx, func(br *types.BundleResult) {
+		t.Log("receive bundle result")
 		resultCh <- br
 	})
 	assert.NoError(t, err)
@@ -65,17 +68,81 @@ func TestSuccessfulClient(t *testing.T) {
 				},
 				ExpirationNs: nil,
 			})
-			assert.NoErrorf(t, err, "failed to send bundle: %v", err)
-			assert.NotNilf(t, res, "response is nil")
-			t.Logf("got bundle response: %v", res)
 
-		// Wait for bundle result
-		case res := <-resultCh:
-			t.Logf("got bundle result")
-			t.Logf("result: %v", res)
-			assert.NotNil(t, res)
+			assert.Error(t, err)
+			assert.ErrorContains(t, err, "rpc error: code = InvalidArgument desc = Invalid bundle, tip amount is 0")
+			assert.Nil(t, res)
+
+			cancel()
 			return
 		}
 	}
+}
 
+func TestMainnetSuccessfulClient(t *testing.T) {
+	ctx := context.Background()
+	subCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	client := sova.NewMainnetClient()
+
+	privKeyB64 := "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+
+	privateKey, err := base64.StdEncoding.DecodeString(privKeyB64)
+	assert.NoError(t, err)
+
+	// Authenticate
+	token, err := client.Authenticate(ctx, privateKey)
+
+	// Expect error: rpc error: code = Unauthenticated desc = Non-whitelisted public key
+	assert.Error(t, err)
+	assert.ErrorContains(t, err, "rpc error: code = Unauthenticated desc = Non-whitelisted public key")
+
+	// Create a new searcher
+	searcher, err := client.Searcher(ctx)
+	assert.NoError(t, err)
+	assert.NotNil(t, searcher)
+	searcher.SetAccessToken(token)
+
+	mempoolCh := make(chan *types.MempoolPacket, 1)
+	resultCh := make(chan *types.BundleResult, 1)
+
+	// Subscribe to mempool packets
+	err = searcher.SubscribeByWorkchain(subCtx, 0, func(mp *types.MempoolPacket) {
+		t.Log("receive mempool packet")
+		mempoolCh <- mp
+	})
+	assert.NoError(t, err)
+
+	// Subscribe to bundle results
+	err = searcher.SubscribeBundleResult(subCtx, func(br *types.BundleResult) {
+		t.Log("receive bundle result")
+		resultCh <- br
+	})
+	assert.NoError(t, err)
+
+	for {
+		select {
+		// Wait for mempool packet
+		case msg := <-mempoolCh:
+			t.Logf("got mempool packet: %v", len(msg.ExternalMessages))
+
+			// Send bundle
+			res, err := searcher.SendBundle(ctx, &types.Bundle{
+				Message: []*types.ExternalMessage{ // Correct type
+					{
+						Data: []byte(msg.ExternalMessages[0].Data),
+					},
+				},
+				ExpirationNs: nil,
+			})
+
+			assert.Error(t, err)
+			assert.ErrorContains(t, err, "rpc error: code = InvalidArgument desc = Invalid bundle, tip amount is 0")
+			assert.Nil(t, res)
+
+			cancel()
+			return
+		}
+	}
 }
